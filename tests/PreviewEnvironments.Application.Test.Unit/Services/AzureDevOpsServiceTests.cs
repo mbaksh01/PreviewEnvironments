623647544ac94fe94a409c186bd3e8de438b6a64
Moe <mbaksh@redev.co.uk>
2024-01-23 20:32:49 +0000

    Started adding tests for AzureDevOpsService.cs

---
+++ tests/PreviewEnvironments.Application.Test.Unit/Services/AzureDevOpsServiceTests.cs
@@ -0,0 +1,132 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using PreviewEnvironments.Application.Models;
+using PreviewEnvironments.Application.Models.AzureDevOps;
+using PreviewEnvironments.Application.Models.AzureDevOps.Contracts;
+using PreviewEnvironments.Application.Services;
+using PreviewEnvironments.Application.Services.Abstractions;
+using PreviewEnvironments.Application.Test.Unit.TestHelpers;
+
+namespace PreviewEnvironments.Application.Test.Unit.Services;
+
+public class AzureDevOpsServiceTests
+{
+    private const string DefaultAccessToken = "my-test-access-token";
+    
+    private readonly string _expectedAccessTokenHeaderValue =
+        Convert.ToBase64String(Encoding.ASCII.GetBytes($":{DefaultAccessToken}"));
+    
+    [Fact]
+    public async Task PostPreviewAvailableMessageAsync_Should_Use_Correct_Http_Request_Message()
+    {
+        // Arrange
+        (IAzureDevOpsService sut, MockHttpMessageHandler messageHandler) =
+            GetSystemUnderTest();
+
+        const string expectedScheme = "https";
+        const string expectedHost = "dev.azure.com";
+        const int expectedPullRequestNumber = 10;
+        const string expectedOrganization = "MyTestOrganization";
+        const string expectedProject = "MyTestProject";
+        Guid expectedRepositoryId = Guid.NewGuid();
+
+        string expectedPath =
+            $"/{expectedOrganization}/{expectedProject}/_apis/git/repositories/{expectedRepositoryId}/pullRequests/{expectedPullRequestNumber}/threads";
+        
+        PreviewAvailableMessage message = new()
+        {
+            Scheme = expectedScheme,
+            Host = expectedHost,
+            PullRequestNumber = expectedPullRequestNumber,
+            Organization = expectedOrganization,
+            Project = expectedProject,
+            RepositoryId = expectedRepositoryId
+        };
+
+        // Act
+        await sut.PostPreviewAvailableMessageAsync(message);
+
+        // Assert
+        messageHandler.Messages.Should().HaveCount(1);
+
+        (HttpRequestMessage actualMessage, string actualContent) =
+            messageHandler.Messages[0];
+        
+        Uri? actualUri = actualMessage.RequestUri;
+
+        actualUri.Should().NotBeNull();
+
+        using (new AssertionScope())
+        {
+            actualMessage.Method.Should().Be(HttpMethod.Post);
+            actualUri!.Scheme.Should().Be(expectedScheme);
+            actualUri!.Host.Should().Be(expectedHost);
+            actualUri!.AbsolutePath.Should().Be(expectedPath);
+            actualMessage.Content.Should().NotBeNull();
+        }
+
+        AuthenticationHeaderValue? header = actualMessage.Headers.Authorization;
+
+        header.Should().NotBeNull();
+
+        using (new AssertionScope())
+        {
+            header!.Scheme.Should().Be("Basic");
+            header.Parameter.Should().Be(_expectedAccessTokenHeaderValue);
+        }
+
+        PullRequestThreadRequest? request =
+            JsonSerializer.Deserialize<PullRequestThreadRequest>(actualContent);
+
+        request.Should().NotBeNull();
+
+        request!.Status.Should().Be("closed");
+        
+        request.Comments.Should().HaveCount(1);
+
+        request.Comments[0].CommentType.Should().Be("system");
+    }
+    
+    [Fact]
+    public async Task PostPreviewAvailableMessageAsync_Should_Not_Throw_When_Status_Code_Does_Not_Indicate_Success()
+    {
+        // Arrange
+        (IAzureDevOpsService sut, _) =
+            GetSystemUnderTest(statusCode: HttpStatusCode.InternalServerError);
+
+        PreviewAvailableMessage message = new();
+
+        // Act
+        Func<Task> action = () => sut.PostPreviewAvailableMessageAsync(message);
+
+        // Assert
+        await action.Should().NotThrowAsync();
+    }
+
+    private static (IAzureDevOpsService, MockHttpMessageHandler) GetSystemUnderTest(
+        string response = "",
+        HttpStatusCode statusCode = HttpStatusCode.OK,
+        ApplicationConfiguration? configuration = null)
+    {
+        configuration ??= new ApplicationConfiguration
+        {
+            AzureDevOps = new AzureDevOpsConfiguration
+            { 
+                AzAccessToken = DefaultAccessToken,
+            }
+        };
+
+        MockHttpMessageHandler messageHandler = new(response, statusCode);
+
+        IAzureDevOpsService sut = new AzureDevOpsService(
+            Substitute.For<ILogger<AzureDevOpsService>>(),
+            Options.Create(configuration),
+            new HttpClient(messageHandler));
+        
+        return (sut, messageHandler);
+    }
+}
---
+++ tests/PreviewEnvironments.Application.Test.Unit/TestHelpers/MockHttpMessageHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Net;
+
+namespace PreviewEnvironments.Application.Test.Unit.TestHelpers;
+
+public class MockHttpMessageHandler : HttpMessageHandler
+{
+    private readonly string _response;
+    private readonly HttpStatusCode _statusCode;
+
+    public List<(HttpRequestMessage Message, string Content)> Messages { get; } = [];
+
+    public MockHttpMessageHandler()
+    {
+        _response = string.Empty;
+        _statusCode = HttpStatusCode.OK;
+    }
+    
+    public MockHttpMessageHandler(string response, HttpStatusCode statusCode)
+    {
+        _response = response;
+        _statusCode = statusCode;
+    }
+
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        if (request.Content is not null)
+        {
+            Messages.Add((Clone(request), await request.Content.ReadAsStringAsync(cancellationToken)));
+        }
+        else
+        {
+            Messages.Add((Clone(request), string.Empty));
+        }
+        
+        return new HttpResponseMessage
+        {
+            StatusCode = _statusCode,
+            Content = new StringContent(_response)
+        };
+    }
+
+    private static HttpRequestMessage Clone(HttpRequestMessage message)
+    {
+        HttpRequestMessage clone = new(message.Method, message.RequestUri)
+        {
+            Content = message.Content,
+            Version = message.Version,
+            VersionPolicy = message.VersionPolicy,
+        };
+
+        foreach (KeyValuePair<string, IEnumerable<string>> header in message.Headers)
+        {
+            clone.Headers.Add(header.Key, header.Value);
+        }
+
+        foreach (KeyValuePair<string, object?> option in message.Options)
+        {
+            clone.Options.TryAdd(option.Key, option.Value);
+        }
+
+        return clone;
+    }
+}
