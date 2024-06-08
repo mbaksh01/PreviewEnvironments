54e8d385484d9543517933c7b94446019350db4c
Moe <mbaksh@redev.co.uk>
2024-06-09 00:23:03 +0100

    Added redirect tests.

---
+++ tests/PreviewEnvironments.Application.Test.Unit/Features/RedirectFeatureTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Extensions.Logging;
+using PreviewEnvironments.Application.Features;
+using PreviewEnvironments.Application.Features.Abstractions;
+using PreviewEnvironments.Application.Models.Docker;
+using PreviewEnvironments.Application.Services.Abstractions;
+
+namespace PreviewEnvironments.Application.Test.Unit.Features;
+
+public class RedirectFeatureTests
+{
+    private readonly IRedirectFeature _sut;
+    private readonly IRedirectService _redirectService;
+    private readonly IContainerTracker _containerTracker;
+    private readonly IDockerService _dockerService;
+    
+    public RedirectFeatureTests()
+    {
+        _redirectService = Substitute.For<IRedirectService>();
+        _containerTracker = Substitute.For<IContainerTracker>();
+        _dockerService = Substitute.For<IDockerService>();
+        
+        _sut = new RedirectFeature(
+            Substitute.For<ILogger<RedirectFeature>>(),
+            _redirectService,
+            _containerTracker,
+            _dockerService);
+    }
+    
+    [Fact]
+    public async Task GetRedirectUriAsync_Should_Return_Correct_Uri()
+    {
+        // Arrange
+        string id = new('A', 12);
+        Uri expectedUri = new("https://test.application.com");
+
+        _containerTracker
+            .SingleOrDefault(Arg.Any<Predicate<DockerContainer>>())
+            .Returns(new DockerContainer
+            {
+                ContainerId = id,
+                ImageName = string.Empty,
+                ImageTag = string.Empty
+            });
+        
+        _redirectService
+            .GetRedirectUri(id)
+            .Returns(expectedUri);
+
+        // Act
+        Uri? actualUri = await _sut.GetRedirectUriAsync(id);
+
+        // Assert
+        actualUri.Should().BeEquivalentTo(expectedUri);
+    }
+    
+    [Fact]
+    public async Task GetRedirectUriAsync_Should_Return_Null_If_Container_Is_Not_Found()
+    {
+        // Arrange
+        string id = new('A', 12);
+
+        // Act
+        Uri? actualUri = await _sut.GetRedirectUriAsync(id);
+
+        // Assert
+        actualUri.Should().BeNull();
+    }
+    
+    [Fact]
+    public async Task GetRedirectUriAsync_Should_Return_Start_Container_When_Expired()
+    {
+        // Arrange
+        string id = new('A', 12);
+        bool startContainerResponse = true;
+        DockerContainer container = new()
+        {
+            ContainerId = id,
+            ImageName = string.Empty,
+            ImageTag = string.Empty,
+            Expired = true,
+        };
+        
+        _containerTracker
+            .SingleOrDefault(Arg.Any<Predicate<DockerContainer>>())
+            .Returns(container);
+
+        _dockerService
+            .StartContainerAsync(id)
+            .Returns(startContainerResponse);
+
+        // Act
+        _ = await _sut.GetRedirectUriAsync(id);
+
+        // Assert
+        await _dockerService
+            .Received(1)
+            .StartContainerAsync(id);
+
+        container.Expired.Should().BeFalse();
+        container.CreatedTime.Should().BeCloseTo(DateTimeOffset.UtcNow, precision: TimeSpan.FromSeconds(1));
+    }
+}
---
+++ tests/PreviewEnvironments.Application.Test.Unit/Services/RedirectServiceTests.cs
@@ -0,0 +1,46 @@
+﻿using PreviewEnvironments.Application.Services;
+using PreviewEnvironments.Application.Services.Abstractions;
+
+namespace PreviewEnvironments.Application.Test.Unit.Services;
+
+public class RedirectServiceTests
+{
+    private readonly IRedirectService _sut = new RedirectService();
+
+    [Fact]
+    public void Add_Should_Return_Correct_Uri()
+    {
+        // Arrange
+        string key = new('A', 12);;
+
+        // Act
+        Uri actualUri = _sut.Add(
+            key,
+            new Uri("https://temp.uri.com"),
+            new Uri("https://test.application.com"));
+
+        // Assert
+        actualUri
+            .Should()
+            .BeEquivalentTo(new Uri($"https://test.application.com/environments/{key}"));
+    }
+    
+    [Fact]
+    public void GetRedirectUri_Should_Return_Correct_Uri()
+    {
+        // Arrange
+        string key = new('A', 12);
+        Uri expectedUri = new("https://temp.uri.com");
+        
+        _sut.Add(
+            key,
+            expectedUri,
+            new Uri("https://test.application.com"));
+        
+        // Act
+        Uri? actualUri = _sut.GetRedirectUri(key);
+
+        // Assert
+        actualUri.Should().BeEquivalentTo(expectedUri);
+    }
+}
